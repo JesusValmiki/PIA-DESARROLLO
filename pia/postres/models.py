25f564fb81682e6937f0c36732d258152ada581f
JesusValmiki <Jesusvalmiki@mail.com>
2023-05-12 19:34:23 -0600

    first commit

---
+++ pia/postres/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Producto(models.Model):
+    descripcion = models.CharField(max_length=60)
+    presentacion = models.CharField(max_length=45)
+    precio = models.FloatField()
+
+    def __str__(self):
+        return self.descripcion
+
+class Equipo(models.Model):
+    nombre = models.CharField(max_length=60)
+    marca = models.CharField(max_length=45)
+    potencia = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
